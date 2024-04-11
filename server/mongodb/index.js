f13bdf2cb8f053f1500e2433d721b55243d743c0
qy <595870773@qq.com>
2024-04-11 13:30:57 +0800

    🐛node 去掉密码规则

--- server/mongodb/index.js
+++ server/mongodb/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true) // 开启严格模式
-mongoose.connect('mongodb://0f2a134cc6c5.c.methodot.com:30544/vue3admin', {})
+// mongoose.connect('mongodb://0f2a134cc6c5.c.methodot.com:30544/vue3admin', {})
+mongoose.connect('mongodb://127.0.0.1:27017', {})
 // mongoose.connect('mongodb+srv://qinyu:13512319102.@qycluster.xsk1hfo.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
