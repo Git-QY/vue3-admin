4234dd0b654565eb716ead1b7740292ddf66727f
qy <595870773@qq.com>
2024-04-15 11:10:28 +0800

    ✨ 拆分layout布局页面

---
+++ src/views/layout/LayoutColumns/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- 分栏布局 -->
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/layout/LayoutVertical/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 纵向布局 -->
+  <div class="layout layout-vertical">
+    <el-container>
+      <el-aside width="200px"><Logo /><Aside /></el-aside>
+      <el-container>
+        <el-header> <Header /></el-header>
+        <el-main><NavTab /> <Main /></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from '../components/header/index.vue'
+import Logo from '../components/logo/index.vue'
+import Aside from '../components/aside/index.vue'
+import Main from '../components/main/index.vue'
+import NavTab from '../components/navTab/index.vue'
+</script>
+
+<style lang="scss"></style>
