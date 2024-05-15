cd304934d5cd6ecf4c95258ca31fd190a3d5f363
韩登勇 <2879405513@qq.com>
2024-05-15 16:07:31 +0800

    小优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { ElMessage, ElMessageBox } from "element-plus"
+
 // 获取assets静态图片
 export const getAssetsImge = (url: string) => {
   return new URL(`../assets/${url}`, import.meta.url).href
@@ -124,3 +126,37 @@ export function getLabel(data: { value: any; label: string }[], value: any, isIt
     return value && value.toString()
   }
 }
+
+/**
+ * @description 操作单条数据信息 (二次确认【删除、禁用、启用、重置密码】)
+ * @param {Function} api 操作数据接口的api方法 (必传)
+ * @param {Object} params 携带的操作数据参数 {id,params} (必传)
+ * @param {String} message 提示信息 (必传)
+ * @returns {Promise}
+ */
+export const useHandleData = (
+  api: (params: any) => Promise<any>,
+  params: any = {},
+  message: string,
+) => {
+  return new Promise((resolve, reject) => {
+    ElMessageBox.confirm(`是否${message}?`, "温馨提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning",
+      draggable: true
+    })
+      .then(async () => {
+        const res = await api(params);
+        if (!res) return reject(false);
+        ElMessage({
+          type: "success",
+          message: `${message}成功!`
+        });
+        resolve(true);
+      })
+      .catch(() => {
+        console.log("取消操作");
+      });
+  });
+};
